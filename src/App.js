1db0836ba5fa65e1076c1464b0be5a4063d38181
Bhavya Sangars <bhavya-sangars@teletracking.com>
2019-04-30 18:23:07 -0400

    added layout for project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Todos from './main/main';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Todos />
-        <p>
-          Todo react app for the <code>Purpose to learn</code> react and redux.
-        </p>
-        </header>
+      <Todos />
     </div>
   );
 }
--- src/main/main.jsx
+++ src/main/main.jsx
@@ -1,14 +1,51 @@
 import React, { Component } from 'react';
+import { Container, Card, CardTitle, Row, Col, CardText, Button } from 'reactstrap';
+import './main.css'
 
 class Todos extends Component {
-    state = {  }
-    render() { 
+    state = {}
+    render() {
         return (
-        <div>
-            <li className='Todo'>Todos list</li>
-        </div>
+            <div>
+                <Button color="primary" size="lg" active>+</Button>
+                <Container>
+                    <Row>
+                        <Col sm="4">
+                        <div className="headerClass"> 
+                            <h3>TODOS</h3>
+                        </div>
+                            <Card body>
+                                <CardTitle>Chores</CardTitle>
+                                <CardText>PowerBill have to be paid by Month end</CardText>
+                                <Button color="primary">Go Inprogress</Button>
+                            </Card>
+                            <Card body>
+                                <CardTitle>Chores</CardTitle>
+                                <CardText>PowerBill have to be paid by Month end</CardText>
+                                <Button color="primary">Go Inprogress</Button>
+                            </Card>
+                        </Col>
+                        <Col sm="4">
+                        <div className="headerClass"><h3>IN PROGRESS</h3></div>
+                            <Card body>
+                                <CardTitle>Chores</CardTitle>
+                                <CardText>PowerBill have to be paid by Month end</CardText>
+                                <Button color="primary">Go Done</Button>
+                            </Card>
+                        </Col>
+                        <Col sm="4">
+                        <div className="headerClass"><h3>DONE</h3></div>
+                            <Card body>
+                                <CardTitle>Chores</CardTitle>
+                                <CardText>PowerBill have to be paid by Month end</CardText>
+                                <Button color="primary">Completed</Button>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         );
     }
 }
- 
+
 export default Todos;
